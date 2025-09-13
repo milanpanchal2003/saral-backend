028bb57fcef2210b2dc4251bdc2146979186d570
mohammad zeeshan <mohammadjeeshan9917@gmail.com>
2025-09-13 10:20:23 +0530

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   message: { type: String, required: true },
+  date: { type: Date, default: Date.now } // yaha auto date save hoga
 });
 
 export default mongoose.model("User", userSchema);
